7ab85331fb06cdfe75d939656e7baa2326bac14b
Lennart Kats <lclkats@gmail.com>
2011-08-12 14:31:26 +0000

    don't include strategoxt.jar in debug plugin
    svn path=/spoofax-imp/trunk/org.strategoxt.imp.debug.stratego.core/; revision=23214

--- org.strategoxt.imp.debug.stratego.core/src/org/strategoxt/debug/core/control/events/IEventInfoExtractorFactory.java
+++ org.strategoxt.imp.debug.stratego.core/src/org/strategoxt/debug/core/control/events/IEventInfoExtractorFactory.java
@@ -1,5 +1,7 @@
 package org.strategoxt.debug.core.control.events;
 
+import org.strategoxt.imp.debug.stratego.runtime.strategies.HybridInterpreterDebugRuntime;
+
 import com.sun.jdi.event.BreakpointEvent;
 import com.sun.jdi.event.MethodEntryEvent;
 import com.sun.jdi.event.MethodExitEvent;
@@ -8,7 +10,7 @@ public class IEventInfoExtractorFactory {
 
 	public static IEventInfoExtractor createExtractor(BreakpointEvent event) {
 		String name = event.location().method().declaringType().name();
-		if (org.strategoxt.imp.debug.stratego.runtime.strategies.HybridInterpreterDebugRuntime.class.getName().equals(name))
+		if (HybridInterpreterDebugRuntime.class.getName().equals(name))
 		{
 			// the breakpoint in the HybridInterpreter does not have any StrategoState info, it is hit before the program is executed
 			return new EmptyExtractor(event);
