621e402376d50190724b01db8cca64beb4c6ff6d
Moritz Lichter <lichtemo@googlemail.com>
2015-10-26 18:14:01 +0100

    Adapt InteropRegisterer

--- org.strategoxt.imp.debug.stratego.runtime/editor/java/org/strategoxt/imp/debug/stratego/runtime/strategies/InteropRegisterer.java
+++ org.strategoxt.imp.debug.stratego.runtime/editor/java/org/strategoxt/imp/debug/stratego/runtime/strategies/InteropRegisterer.java
@@ -1,7 +1,6 @@
 package org.strategoxt.imp.debug.stratego.runtime.strategies;
 
 import org.strategoxt.lang.JavaInteropRegisterer;
-import org.strategoxt.lang.Strategy;
 
 /**
  * Helper class for {@link java_strategy_0_0}.
@@ -9,14 +8,6 @@ import org.strategoxt.lang.Strategy;
 public class InteropRegisterer extends JavaInteropRegisterer {
 
 	public InteropRegisterer() {
-		super(new Strategy[] { 
-				java_s_enter_0_4.instance // strategy enter
-				, java_s_exit_0_4.instance // strategy exit
-				, java_r_enter_0_4.instance // rule enter
-				, java_r_exit_0_4.instance // rule exit
-				, java_s_step_0_4.instance // step
-				, java_s_var_0_5.instance // var assignment
-				, java_s_fail_0_4.instance // where/with clause fails
-				});
+		super(new LibraryInitializer());
 	}
 }
--- org.strategoxt.imp.debug.stratego.transformer/editor/java/org/strategoxt/imp/debug/stratego/transformer/strategies/InteropRegisterer.java
+++ org.strategoxt.imp.debug.stratego.transformer/editor/java/org/strategoxt/imp/debug/stratego/transformer/strategies/InteropRegisterer.java
@@ -6,13 +6,6 @@ import org.strategoxt.lang.Strategy;
 public class InteropRegisterer extends JavaInteropRegisterer {
 
 	public InteropRegisterer() {
-		super(new Strategy[] { 
-				java_store_term_0_0.instance
-				, java_store_term_0_1.instance
-				, java_monitor_start_0_1.instance
-				, java_monitor_end_0_1.instance
-				, java_timer_print_0_0.instance
-				, java_create_fll_table_0_3.instance
-		});
+		super(new LibraryInitializer());
 	}
 }
